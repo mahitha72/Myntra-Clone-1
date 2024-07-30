20f2abfc87d8c58aa58b193535830226076c26b6
mahitha72 <mahitha29.ikkurthi@gmail.com>
2024-07-30 14:27:53 +0530

    notify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
-
+import { generateToken, messaging } from "./Notifications/firebase";
+import { useEffect } from "react";
+import { onMessage } from "firebase/messaging";
 import './App.css';
 import AllRoutes from './Components/AllRoutes';
 
 
 function App() {
-  
+  useEffect(() => {
+    generateToken();
+    onMessage(messaging, (payload) => {
+      console.log(payload);
+    });
+  }, []);
   return (
     <div className="App">
      <AllRoutes />
